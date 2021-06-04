09e1560d818fc22e95b095f9758b84e2031b7a55
jeevan15498 <jeevan15498@gmail.com>
2021-06-04 21:48:52 +0530

    updated

--- content/example/example-10-1.php
+++ content/example/example-10-1.php
@@ -10,8 +10,9 @@
         // echo "<span style='color: red;'>Please Fill Form Data.</span>";
       } else {
         if (mt_rand(0, 1) === 1) {
-          echo "<script>alert('Data has been successful saved.');</script>";
+          // echo "<script>alert('Data has been successful saved.');</script>";
           // echo "<span style='color: green;'>Data has been successful saved.</span>";
+          echo "<script>alert('This id already exists. \n\nThis is Demo Page.'); window.location.reload();</script>";
         } else {
           echo "<script>alert('This id already exists. \n\nThis is Demo Page.'); window.location.reload();</script>";
           // echo "<span style='color: red;'>This id already exists</span>";
@@ -20,7 +21,7 @@
     }
   ?>
   <hr>
-  <h3>Example - 10</h3>
+  <h3>Example - 10-1</h3>
   <p style="margin-bottom: 30px">If there is any error (alert message) while saving the entry then we can fill the second entry and the error should be stored in the excel template.</p>
   <hr>
   <?php include_once("./components/form.php") ?>
